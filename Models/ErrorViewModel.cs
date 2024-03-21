7e0d7d27aab35b9d036e61213622f6b620dc1a08
NGUYEN DAC HOANG DAT <datndhhe170720@fpt.edu.vn>
2024-03-22 02:11:51 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_PRN221_BookingField.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
